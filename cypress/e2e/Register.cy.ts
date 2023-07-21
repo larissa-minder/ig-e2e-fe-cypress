b6590798e4f8f67d525fe4a72caaf5afb5b584d7
Larissa Pimenta <larissa.pimenta@kingfisher.com>
2023-07-21 13:47:08 +0100

    remove fail test

--- cypress/e2e/Register.cy.ts
+++ cypress/e2e/Register.cy.ts
@@ -6,14 +6,14 @@ const register = new Register();
 describe('Register User', () => {
   
 
-  it('Failed User Registration ', () => {
+ /* it('Failed User Registration ', () => {
     cy.visit('/')
     topBar.GoToRegisterPage();
     var email = 'random' + Date.now() + '@gmail.com';
     register.RegisterForm('test','lastmame',email,'315782804', 'test!@154','test!@154' );
     register.accountCreatedMessage('Your Account Has Been Created! error');
 
-  })
+  })*/
 
   it('Successfull User Registration ', () => {
     cy.visit('/')
